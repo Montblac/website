35804e88c1ab1bef65c3bd33c85738722666a050
Montblac <Montblac@users.noreply.github.com>
2020-11-17 20:52:03 -0800

    Modify contact.js and hero.js

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -51,8 +51,8 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I'm not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I'll try my best to get back to you!
+        I am actively seeking new opportunities and would be excited to hear from you. Drop by my
+        inbox for any questions or remarks, and I'll try my best to get back to you.
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -48,12 +48,12 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Sam Leyva.</h2>;
+  const three = <h3 className="big-heading">I am a tech enthusiast.</h3>;
   const four = (
     <p>
-      I'm a software engineer based in Boston, MA specializing in building (and occasionally
-      designing) exceptional websites, applications, and everything in between.
+      I'm a Computer Science graduate looking to expand my software development skills, work with
+      all sorts of tech, and build some exciting projects.
     </p>
   );
   const five = (
